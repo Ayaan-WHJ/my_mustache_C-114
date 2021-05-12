06ef7fb2b41a82a53391d92f5fcafb499e2d5c3e
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-05-12 17:31:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+noseX = 0;
+noseY = 0;
 function preload()
 {
-
+  moustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup()
 {
-    canvas = createCanvas(500,500);
+    canvas = createCanvas(300,300);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(300,300);
@@ -14,24 +16,30 @@ function setup()
     poseNet.on('pose',gotPoses);
 }
 
-function draw()
-{image(video,0,0,500,500);
+function draw(){ 
+    image(video,0,0,300,300);
+   
+    image(moustache,noseX,noseY,30,30);
 }
 
 function take_snapshot()
 {
-save('my_mustache.png');
+save('Mymoustache.png');
 }
 
-function modelLoaded(){
-console.log('PoseNet is initialized');}
+function modelLoaded()
+{
+    console.log('PoseNet is initialized');
+}
 
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x =" + results[0].pose.nose.x);
-        console.log("nose y =" + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y+2;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
     }
 }
